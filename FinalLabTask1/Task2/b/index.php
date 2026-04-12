efc87ee36d5837f5438f5dd8b8ff8e12ddb6d329
monibur-emu <moniburrahmanemu@gmail.com>
2026-04-12 14:57:43 +0600

    adding task2 b

--- FinalLabTask1/Task2/b/index.php
+++ FinalLabTask1/Task2/b/index.php
@@ -1,21 +1,19 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "Name: " . $_POST['name'];
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>name</title>
+    <title>email</title>
 </head>
 <body> <form method="post" action="index.php">
-    <input type="text" name="name" >
+      email:  <input type="email" name="email" >
     <input type="submit" name="submit" value="Submit">
 </form>
 </body></html>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "email: " . $_POST['email'];
+}
+?>
