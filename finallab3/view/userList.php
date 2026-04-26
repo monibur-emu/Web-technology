fc9f09233d9e11e7964cd1e9f1a5eb9b8d9a9c86
monibur-emu <moniburrahmanemu@gmail.com>
2026-04-26 14:50:19 +0600

    update userlist

--- finallab3/view/userList.php
+++ finallab3/view/userList.php
@@ -1,47 +1,20 @@
-<?php
-include("session.php");
-
-if (!isset($_SESSION['status'])) {
-    header("Location: login.php");
-    exit();
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User List</title>
-</head>
-<body>
+<?php include("../asset/session.php"); ?>
 
 <h2>User List</h2>
 
-<p>
-    <a href="productDashboard.php">Back to Dashboard</a> |
-    <a href="logout.php">Logout</a>
-</p>
+<a href="productDashboard.php">Back</a>
 
-<hr>
+<table border="1">
+<tr><th>User</th><th>Action</th></tr>
 
-<table border="1" cellpadding="10">
-    <tr>
-        <th>Username</th>
-        <th>Actions</th>
-    </tr>
-
-    <?php
-    foreach ($_SESSION['users'] as $username => $user) {
-    ?>
-        <tr>
-            <td><?php echo $username; ?></td>
-            <td>
-                <a href="userDetails.php?username=<?php echo $username; ?>">Details</a> |
-                <a href="editUser.php?username=<?php echo $username; ?>">Edit</a> |
-                <a href="deleteUser.php?username=<?php echo $username; ?>">Delete</a>
-            </td>
-        </tr>
-    <?php } ?>
+<?php foreach ($_SESSION['users'] as $u => $data) { ?>
+<tr>
+<td><?= $u ?></td>
+<td>
+    <a href="userDetails.php?username=<?= $u ?>">Details</a> |
+    <a href="editUser.php?username=<?= $u ?>">Edit</a> |
+    <a href="../controller/deleteUser.php?username=<?= $u ?>">Delete</a>
+</td>
+</tr>
+<?php } ?>
 </table>
-
-</body>
-</html>
