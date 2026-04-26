b0b00e03efcf2304afafedfa82b57fa7f7f674a7
monibur-emu <moniburrahmanemu@gmail.com>
2026-04-26 14:16:09 +0600

    updated dashboard

--- finallab3/view/productdashboard.php
+++ finallab3/view/productdashboard.php
@@ -1,14 +1,14 @@
 <?php
 include("session.php");
 
-if (!isset($_SESSION['status']) || $_SESSION['status'] !== true) {
+if (!isset($_SESSION['status'])) {
     header("Location: login.php");
     exit();
 }
 
-$username = $_SESSION['username'];
 $message = "";
 
+/* ADD PRODUCT */
 if (isset($_POST['add'])) {
     $id = $_POST['id'];
     $name = $_POST['name'];
@@ -22,34 +22,9 @@ if (isset($_POST['add'])) {
             "price" => $price,
             "quantity" => $quantity
         ];
-        $message = "Product added successfully.";
+        $message = "Product added successfully!";
     } else {
-        $message = "Please fill all fields.";
-    }
-}
-
-if (isset($_POST['update'])) {
-    $id = $_POST['id'];
-
-    if (isset($_SESSION['products'][$id])) {
-        $_SESSION['products'][$id]['name'] = $_POST['name'];
-        $_SESSION['products'][$id]['price'] = $_POST['price'];
-        $_SESSION['products'][$id]['quantity'] = $_POST['quantity'];
-
-        $message = "Product updated successfully.";
-    } else {
-        $message = "Product not found.";
-    }
-}
-
-if (isset($_POST['delete'])) {
-    $id = $_POST['id'];
-
-    if (isset($_SESSION['products'][$id])) {
-        unset($_SESSION['products'][$id]);
-        $message = "Product deleted successfully.";
-    } else {
-        $message = "Product not found.";
+        $message = "All fields required!";
     }
 }
 ?>
@@ -64,13 +39,14 @@ if (isset($_POST['delete'])) {
 <h2>Product Dashboard</h2>
 
 <p>
-    Logged in as <b><?php echo $username; ?></b> |
+    Welcome, <?php echo $_SESSION['username']; ?> |
     <a href="logout.php">Logout</a>
 </p>
 
 <hr>
 
-<h3>Add / Update / Delete Product</h3>
+<!-- ADD PRODUCT -->
+<h3>Add Product</h3>
 
 <p><?php echo $message; ?></p>
 
@@ -78,25 +54,24 @@ if (isset($_POST['delete'])) {
     Product ID<br>
     <input type="text" name="id"><br><br>
 
-    Product Name<br>
+    Name<br>
     <input type="text" name="name"><br><br>
 
-    Product Price<br>
+    Price<br>
     <input type="number" name="price"><br><br>
 
-    Product Quantity<br>
+    Quantity<br>
     <input type="number" name="quantity"><br><br>
 
     <input type="submit" name="add" value="Add Product">
-    <input type="submit" name="update" value="Update Product">
-    <input type="submit" name="delete" value="Delete Product">
 </form>
 
 <hr>
 
+<!-- PRODUCT LIST -->
 <h3>Product List</h3>
 
-<table border="1" cellpadding="8" cellspacing="0">
+<table border="1" cellpadding="10">
     <tr>
         <th>ID</th>
         <th>Name</th>
@@ -104,14 +79,22 @@ if (isset($_POST['delete'])) {
         <th>Quantity</th>
     </tr>
 
-    <?php foreach ($_SESSION['products'] as $product) { ?>
+    <?php
+    if (!empty($_SESSION['products'])) {
+        foreach ($_SESSION['products'] as $product) {
+    ?>
         <tr>
             <td><?php echo $product['id']; ?></td>
             <td><?php echo $product['name']; ?></td>
             <td><?php echo $product['price']; ?></td>
             <td><?php echo $product['quantity']; ?></td>
         </tr>
-    <?php } ?>
+    <?php
+        }
+    } else {
+        echo "<tr><td colspan='4'>No products available</td></tr>";
+    }
+    ?>
 </table>
 
 </body>
